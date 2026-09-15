dd8593ec81efe943f3bb654f0ab9615600afab41
sauravmanisp99-alt <sauravmanisp99@gmail.com>
2026-09-15 11:20:14 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD9_E8TZT1K3pQOBKvqyh9HvMA-D0SoiLc",
+  authDomain: "milo-india-b7415.firebaseapp.com",
+  projectId: "milo-india-b7415",
+  storageBucket: "milo-india-b7415.firebasestorage.app",
+  messagingSenderId: "224921392601",
+  appId: "1:224921392601:web:303024d4f5e1d78f9635c8"
+};
